89799342e2e88474829c6e2ee8a8006245d9d29e
jassam256 <jassam.25g@gmail.com>
2026-05-28 15:59:07 +1000

    menu page
    added menu page and some css functionality

---
+++ .a_rebuild/js/menu.js
@@ -0,0 +1,37 @@
+// Menu navigation
+const drinksBtn = document.getElementById("drinks-btn");
+const foodBtn = document.getElementById("food-btn");
+const dessertBtn = document.getElementById("dessert-btn");
+
+// Event Listeners
+drinksBtn.addEventListener("click", () => openForm("drinks-menu"));
+foodBtn.addEventListener("click", () => openForm("food-menu"));
+dessertBtn.addEventListener("click", () => openForm("dessert-menu"));
+
+function openForm(target) {
+  let targetForm = document.getElementById(target);
+
+  if (targetForm.classList.contains("hide")) {
+    closeForms();
+
+    targetForm.classList.remove("hide");
+  }
+}
+
+function closeForms() {
+  const drinks = document.getElementById("drinks-menu");
+  const food = document.getElementById("food-menu");
+  const dessert = document.getElementById("dessert-menu");
+
+  if (!drinks.classList.contains("hide")) {
+    drinks.classList.add("hide");
+  }
+
+  if (!food.classList.contains("hide")) {
+    food.classList.add("hide");
+  }
+
+  if (!dessert.classList.contains("hide")) {
+    dessert.classList.add("hide");
+  }
+}
--- .a_rebuild/js/script.js
+++ .a_rebuild/js/script.js
@@ -1,9 +1,15 @@
 // NAV MENU TOGGLE
 const toggle = document.getElementById("menu-toggle");
 const navLinks = document.getElementsByClassName("nav-links");
+const menuToggle = document.getElementById("menu-toggle");
 
 toggle.addEventListener("click", () => {
   // console.log("menu click");
+
+  // change symbol
+  if (menuToggle.classList.contains("active")) {
+    menuToggle.classList.remove("active");
+  } else menuToggle.classList.add("active");
 
   // show links
   for (let i = 0; i < navLinks.length; i++) {
