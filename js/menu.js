a690e94ee07bb12501d137bbc971e08e29c12abe
jassam256 <jassam.25g@gmail.com>
2026-05-29 15:22:32 +1000

    replaced original website with rebuilt version
    deleted old website and replaced it with the rebuilt version, with completed features and responsiveness.
    
    built from a mobile-first approach.

--- js/menu.js
+++ js/menu.js
@@ -1,22 +1,24 @@
-// MENU NAVIGATION
+// Menu navigation
 const drinksBtn = document.getElementById("drinks-btn");
 const foodBtn = document.getElementById("food-btn");
 const dessertBtn = document.getElementById("dessert-btn");
 
-// Initially used a click rather than a mouseover
-drinksBtn.addEventListener("mouseover", () => openForm("drinks-menu"));
-foodBtn.addEventListener("mouseover", () => openForm("food-menu"));
-dessertBtn.addEventListener("mouseover", () => openForm("dessert-menu"));
+// Event Listeners
+drinksBtn.addEventListener("click", () => openForm("drinks-menu"));
+foodBtn.addEventListener("click", () => openForm("food-menu"));
+dessertBtn.addEventListener("click", () => openForm("dessert-menu"));
 
+// OPEN AND CLOSE MENU FORMS
 function openForm(target) {
   let targetForm = document.getElementById(target);
+
+  removeActive();
+  addActive(target);
 
   if (targetForm.classList.contains("hide")) {
     closeForms();
 
     targetForm.classList.remove("hide");
-  } else {
-    // targetForm.classList.add("hide");
   }
 }
 
@@ -35,5 +37,33 @@ function closeForms() {
 
   if (!dessert.classList.contains("hide")) {
     dessert.classList.add("hide");
+  }
+}
+
+// REMOVE ACTIVE BUTTON
+function removeActive() {
+  if (drinksBtn.classList.contains("selected")) {
+    drinksBtn.classList.remove("selected");
+  }
+
+  if (foodBtn.classList.contains("selected")) {
+    foodBtn.classList.remove("selected");
+  }
+
+  if (dessertBtn.classList.contains("selected")) {
+    dessertBtn.classList.remove("selected");
+  }
+}
+
+// ADD ACTIVE BUTTON
+function addActive(target) {
+  if (target == "drinks-menu") {
+    drinksBtn.classList.add("selected");
+  }
+  if (target == "food-menu") {
+    foodBtn.classList.add("selected");
+  }
+  if (target == "dessert-menu") {
+    dessertBtn.classList.add("selected");
   }
 }
--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
 // NAV MENU TOGGLE
 const toggle = document.getElementById("menu-toggle");
 const navLinks = document.getElementsByClassName("nav-links");
+const menuToggle = document.getElementById("menu-toggle");
 
 toggle.addEventListener("click", () => {
   // console.log("menu click");
 
-  // change icon
-  if (toggle.textContent == "✕") {
-    toggle.textContent = "☰";
-  } else toggle.textContent = "✕";
+  // change symbol
+  if (menuToggle.classList.contains("active")) {
+    menuToggle.classList.remove("active");
+  } else menuToggle.classList.add("active");
 
   // show links
   for (let i = 0; i < navLinks.length; i++) {
